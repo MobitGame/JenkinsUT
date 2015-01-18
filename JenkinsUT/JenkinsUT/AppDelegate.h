62feb4b9b2adaea51e89151ecfa1d03924479064
Lee Way <leeway@Lees-MacBook-Pro.local>
2015-01-19 00:41:03 +0700

    classes file

---
+++ JenkinsUT/JenkinsUT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsUT
+//
+//  Created by Lee Way on 1/19/15.
+//  Copyright (c) 2015 Lee Way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsUT/JenkinsUT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsUT
+//
+//  Created by Lee Way on 1/19/15.
+//  Copyright (c) 2015 Lee Way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
