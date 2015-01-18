b41dfc5fa11d79e1c0e8742da5cb7ddc65ed55f3
Lee Way <leeway@Lees-MacBook-Pro.local>
2015-01-19 00:50:10 +0700

    dadas

---
+++ JenkinsUT/JenkinsUT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsUT
+//
+//  Created by Lee Way on 1/19/15.
+//  Copyright (c) 2015 Lee Way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
